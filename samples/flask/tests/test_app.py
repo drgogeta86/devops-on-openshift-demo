dec8c5ebb351f2eea093fc57c59f8a26d045f70c
Francesco Fiore <francescofiore.81@gmail.com>
2018-03-23 16:54:25 +0100

    Fix tests

--- samples/flask/tests/test_app.py
+++ samples/flask/tests/test_app.py
@@ -12,7 +12,7 @@ def app_client():
 
 def test_welcome(app_client):
     response = app_client.get('/api/1.0/welcome')
-    payload = json.loads(response.data)
+    payload = json.loads(response.data.decode('utf-8'))
 
     assert response.status_code == 200
     assert 'message' in payload
